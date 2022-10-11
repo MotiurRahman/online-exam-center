ebf49f892f7a0724abd407d6fb62f3f7ee621a9e
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-10-11 11:02:21 +0600

    Add Header

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      <h3>this is home page</h3>
+      <header className="headerView text-white">
+        <h3>Online Exam System</h3>
+        <p>Now the exam is fun do it anytime anywhere even open book.</p>
+      </header>
+      <main>
+        <section></section>
+      </main>
     </div>
   );
 };
